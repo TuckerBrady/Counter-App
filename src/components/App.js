09b665962a271374db6b947250692ff46f3737cb
TuckerBrady <tuck.al.brady@gmail.com>
2021-07-07 11:58:15 -0600

    initial commit/build.

---
+++ src/components/App.js
@@ -0,0 +1,54 @@
+import React from "react";
+import RecipeList from "./RecipeList";
+import '../components/css/app.css';
+
+
+function App() {
+  return (
+    <RecipeList  recipes={sampleRecipes}/>
+  )
+}
+
+
+const sampleRecipes = [
+  {
+    id: 1,
+    name: 'Plain Chicken',
+    servings: 3,
+    cookTime: '1:45',
+    instructions: "1. Put salt on Chicken\n2. Put chicken in oven\n3. Eat chicken",
+    ingredients: [
+      {
+        id: 1,
+        name: 'Chicken',
+        amount: '2 Pounds'
+      },
+      {
+        id: 2,
+        name: 'Salt',
+        amount: '1 Tbs'
+      },
+    ]
+  },
+  {
+    id: 2,
+    name: 'Plain Pork',
+    servings: 5,
+    cookTime: '0:45',
+    instructions: "1. Put paprika on Pork\n2. Put Pork in oven\n3. Eat Pork",
+    ingredients: [
+      {
+        id: 1,
+        name: 'Pork',
+        amount: '3 Pounds'
+      },
+      {
+        id: 2,
+        name: 'Paprika',
+        amount: '2 Tbs'
+      },
+    ]
+  }
+]
+
+export default App;
