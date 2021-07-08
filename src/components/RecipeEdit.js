fcae3e3ce39209b490860c89b316921e4489248a
TuckerBrady <tuck.al.brady@gmail.com>
2021-07-08 14:36:22 -0600

    hooked up buttons

--- src/components/RecipeEdit.js
+++ src/components/RecipeEdit.js
@@ -1,33 +1,72 @@
 import React from 'react'
 import RecipeIngredientEdit from './RecipeIngredientEdit'
 
-export default function RecipeEdit() {
+export default function RecipeEdit({ recipe }) {
     return (
         <div className="recipe-edit">
-            <div>
-                <button>&times;</button>
+            <div className="recipe-edit__remove-button-container">
+                <button className="btn recipe-edit__remove-button">&times;</button>
             </div>
-            <div>
-                <label htmlFor="name">Name</label>
-                <input type="text" name="name" id="name" />
-                <label htmlFor="cookTime">Cook Time</label>
-                <input type="text" name="cookTime" id="cookTime" />
-                <label htmlFor="servings">Servigs</label>
-                <input type="number" min="1" name="name" id="servings" />
-                <label htmlFor="instructions">Instructions</label>
-                <textarea name="instructions" id="instructions" />
+            <div className="recipe-edit__details-grid">
+                <label 
+                    htmlFor="name" 
+                    className="recipe-edit__label"
+                >Name</label>
+                <input 
+                    type="text" 
+                    name="name" 
+                    id="name" 
+                    value={recipe.name}
+                    className="recipe-edit__input"
+                />
+                <label 
+                    htmlFor="cookTime" 
+                    className="recipe-edit__label"
+                >Cook Time</label>
+                <input 
+                    type="text" 
+                    name="cookTime" 
+                    id="cookTime" 
+                    value={recipe.cookTime}
+                    className="recipe-edit__input"
+                />
+                <label 
+                    htmlFor="servings"
+                    className="recipe-edit__label"
+                >Servigs</label>
+                <input 
+                    type="number" 
+                    min="1" 
+                    name="name" 
+                    id="servings" 
+                    value={recipe.servings}
+                    className="recipe-edit__input"
+                />
+                <label 
+                    htmlFor="instructions"
+                    className="recipe-edit__label"
+                >Instructions</label>
+                <textarea 
+                name="instructions"
+                className="recipe-edit__input" 
+                id="instructions" 
+                >{recipe.instructions}</textarea>
             </div>
             <br />
-            <label>Ingredients</label>
-            <div>
+            <label className="recipe-edit__label">Ingredients</label>
+            <div className="recipe-edit__ingredient-grid">
                 <div>Name</div>
                 <div>Amount</div>
                 <div></div>
-                <RecipeIngredientEdit />
-                <RecipeIngredientEdit />
+                {recipe.ingredients.map(ingredient => (
+                    <RecipeIngredientEdit 
+                        key={ingredient.id} 
+                        ingredient={ingredient} 
+                    />
+                ))}
             </div> 
-            <div>
-                <button>Add Ingredient</button>
+            <div className="recipe-edit__add-ingredient-btn-container">
+                <button className="btn btn--primary">Add Ingredient</button>
             </div>
         </div>
     )
